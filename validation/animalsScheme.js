5da1ef9512d0e79d00085da13216123f2325fe60
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-02-24 20:43:59 -0300

    Add new validation and values to post on database

--- validation/animalsScheme.js
+++ validation/animalsScheme.js
@@ -9,18 +9,12 @@ const animalsScheme = zod.object({
     invalid_type_error: 'type value must be one of the indicated',
     required_error: 'type is required'
   }),
-  description: zod.string(
-    {
-      required_error: 'description is required'
-    }
-  ),
-  imgSrc: zod.string(
-    {
-      required_error: 'imgSrc is required'
-    }
+  description: zod.string({ required_error: 'description is required', invalid_type_error: 'description value must be a string' }),
+  imgSrc: zod.string({ required_error: 'imgSrc is required' }
   ).url(),
-  lat: zod.number(),
-  lng: zod.number()
+  lat: zod.number({ required_error: 'lat es required', invalid_type_error: 'lat value must be a number' }),
+  lng: zod.number({ required_error: 'lng es required', invalid_type_error: 'lng value must be a number' }),
+  address: zod.string({ required_error: 'address es required', invalid_type_error: 'address value must be a string' })
 });
 
 export const validateAnimal = (animal) => {
