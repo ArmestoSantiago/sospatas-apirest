7b41e5e86c9a7fac68b11888b5a1a775deaef082
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-03-03 02:12:38 -0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ import { UsersModel } from '../model/users.js';
 export class UsersController {
   static async getPostedByUser(req, res) {
     try {
-      const userId = req.query;
+      const userId = req.query.userid;
       const postedByUser = await UsersModel.getPostedByUser({ userId });
 
       res.json(postedByUser);
