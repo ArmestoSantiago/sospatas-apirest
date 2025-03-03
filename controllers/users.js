010f80e5f4f0fc33469015b7055e91d829d1f222
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-03-03 02:07:07 -0300

    Change the way to recive user_id

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ import { UsersModel } from '../model/users.js';
 export class UsersController {
   static async getPostedByUser(req, res) {
     try {
-      const userId = req.body.user_id;
+      const userId = req.query;
       const postedByUser = await UsersModel.getPostedByUser({ userId });
 
       res.json(postedByUser);
