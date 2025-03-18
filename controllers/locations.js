b889297268263ff79ca0f103eb48ebfe0474ffdc
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-03-18 11:32:35 -0300

    Fix predictions endpoint. Now should return predictions

--- controllers/locations.js
+++ controllers/locations.js
@@ -3,10 +3,8 @@ import { LocationModel } from '../model/locations.js';
 export class LocationController {
   static async getPredictions(req, res) {
     const { input } = req.query;
-    // const predictions = await LocationModel.getPredictionList({ input });
+    const predictions = await LocationModel.getPredictionList({ input });
 
-    // res.json(predictions);
-
-    res.json({ input });
+    res.json(predictions);
   }
 }
--- model/locations.js
+++ model/locations.js
@@ -6,14 +6,17 @@ export class LocationModel {
   static async getPredictionList({ input }) {
     const PREDICTION_URL = process.env.PREDICTIONS_ENDPOINT;
     const GOOGLE_KEY = process.env.GOOGLE_KEY;
+    const fullURL = `${PREDICTION_URL}input=${input}&key=${GOOGLE_KEY}`;
 
     try {
-      return fetch(`${PREDICTION_URL}input=${input}&key=${GOOGLE_KEY}`)
-        .then(res => res.json())
-        .then(data => data.predictions);
-    } catch (error) {
-      console.log(error);
+      const data = await fetch(fullURL);
+      const res = await data.json();
+      if (res.predictions.length === 0) return false;
+      return res;
+    }
+    catch (error) {
       return false;
     }
+
   }
 }
